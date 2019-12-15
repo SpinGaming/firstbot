4ad0b079b56c9e177357e6f171b90951006a6f85
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-15 01:05:40 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
 const token = 'NjU1NDMwMTExODc2ODc0Mjcx.XfT-7A.H4QGBDzLKZpfk6tqeraLDwLvb6Q';
+const ytdl = require('ytdl-core');
+const streamOptions = {seek: 0, volume};
+const broadcast = client.createVoiceBroadcast();
 
 var PREFIX = '|';
-
 var version = '1.0.1'
-
+var volume = 5
 
 bot.on('ready', () =>{
     console.log('This Bot Is Online!');
@@ -44,6 +45,31 @@ bot.on('message', message=>{
             PREFIX = args[1];
             message.channel.sendMessage('The prefix is now: ' + PREFIX);
             };
+        break;
+
+        case 'play':
+            if(!args[1]){ return message.reply('I need a URL!')} else{
+            voiceChannel.join().then(connection => {const stream = ytdl(msgArgs[1], { filter: 'audioonly'});
+            broadcast.playStream(stream);
+            const dispatcher = connection.playBroadcast(broadcast);
+        }).catch(console.error)};
+        break;
+
+        case 'volume':
+
+            if(!args[1]) return message.channel.sendMessage('Must be between 1-10')
+
+            if(args[1] = 1).then(volume = 1)
+            if(args[1] = 2).then(volume = 2)
+            if(args[1] = 3).then(volume = 3)
+            if(args[1] = 4).then(volume = 4)
+            if(args[1] = 5).then(volume = 5)
+            if(args[1] = 6).then(volume = 6)
+            if(args[1] = 7).then(volume = 7)
+            if(args[1] = 8).then(volume = 8)
+            if(args[1] = 9).then(volume = 9)
+            if(args[1] = 10).then(volume = 10);
+
         break;
     }
 })
