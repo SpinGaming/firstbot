6fc3315e9072ed413f24ea2e4a73f0d54f7e371e
SpinGaming <46069683+SpinGaming@users.noreply.github.com>
2019-12-15 01:39:07 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -5,14 +5,6 @@ const token = 'NjU1NDMwMTExODc2ODc0Mjcx.XfT-7A.H4QGBDzLKZpfk6tqeraLDwLvb6Q';
 var PREFIX = '|';
 var version = '1.0.1'
 
-var totalSeconds = (bot.uptime / 1000);
-var days = Math.floor(totalSeconds / 86400);
-var hours = Math.floor(totalSeconds / 3600);
-totalSeconds %= 3600;
-var minutes = Math.floor(totalSeconds / 60);
-var seconds = totalSeconds % 60;
-
-
 
 bot.on('ready', () =>{
     console.log('This Bot Is Online!');
@@ -38,7 +30,7 @@ bot.on('message', message=>{
             .setColor(0x7289DA)
             .addField('Prefix:', PREFIX,)
             .addField('Version:', version)
-            .addField('Bot Uptime:', days + ':' + hours + ':' + minutes + ':' + seconds)
+            .addField('Bot Uptime:', bot.uptime)
             .addField('Employed In:', message.guild.name, true)
             .addField('Developer:', '@Joshh#0922', true)
             .setThumbnail("https://cdn-images-1.medium.com/max/1600/1*n4-mfREc-TgooqGwu2YHnw.png")
